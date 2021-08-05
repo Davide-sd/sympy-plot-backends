3482826f48e376786a88cf3118b706707dad9cd3
Davide_sd <sandona.davide@gmail.com>
2021-08-05 12:54:32 +0200

    small fixes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,10 +11,6 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 import sys
 import inspect
@@ -22,9 +18,9 @@ import os
 import sys
 import sphinx_rtd_theme
 
-# from: 
-# https://sphinx-rtd-tutorial.readthedocs.io/en/latest/sphinx-config.html#autodoc-configuration
-sys.path.insert(0, os.path.abspath('../../'))
+asd = os.path.abspath('../../spb/')
+print("WTF", asd)
+sys.path.insert(0, asd)
 
 # -- Project information -----------------------------------------------------
 
