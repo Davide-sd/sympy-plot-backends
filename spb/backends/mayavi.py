99fc9dd9da6ca191c895d200e4afc9d281bd8a75
Davide Sandonà <sandona.davide@gmail.com>
2022-08-14 11:48:21 +0200

    fixed bug

--- spb/backends/mayavi.py
+++ spb/backends/mayavi.py
@@ -93,7 +93,9 @@ class MayaviBackend(Plot):
             import_kwargs={'fromlist': ['core']},
             min_module_version='8.4.0',
             catch=(RuntimeError,))
-        self._display = IPython.core.display.display
+        if IPython:
+            # this if statement is required to pass tests with Python 3.7
+            self._display = IPython.core.display.display
 
         self.colorloop = matplotlib.cm.tab10.colors
         self.colormaps = [
