Fixed bug with uniform evaluation of numerical functions
If user provides a numerical lambda function, it will be evaluated by the same algorithm that
evaluates symbolic expressions. This guarantees reproducibility of results.

spb/series.py
@@ -218,14 +218,16 @@ def _uniform_eval(free_symbols, expr, *args, modules=None):
         No matter the evaluation ``modules``, the array type is going to be
         complex.
     """
-    if callable(expr):
-        return expr(*args)
+    if not callable(expr):
+        # generate two lambda functions: the default one, and the backup in
+        # case of failures with the default one.
+        f1 = lambdify(free_symbols, expr, modules=modules)
+        f2 = lambdify(free_symbols, expr, modules="sympy")
+        return _uniform_eval_helper(f1, f2, *args, modules=modules)
 
-    # generate two lambda functions: the default one, and the backup in case
-    # of failures with the default one.
-    f1 = lambdify(free_symbols, expr, modules=modules)
-    f2 = lambdify(free_symbols, expr, modules="sympy")
-    return _uniform_eval_helper(f1, f2, *args, modules=modules)
+    # NOTE: expr is a callable. It is important that it'll be evaluated by
+    # _uniform_eval_helper because it uses np.vectorize
+    return _uniform_eval_helper(expr, None, *args, modules=modules)
 
 
 def _uniform_eval_helper(f1, f2, *args, modules=None):
@@ -240,11 +242,14 @@ def _uniform_eval_helper(f1, f2, *args, modules=None):
             return complex(func(*args))
         except (ZeroDivisionError, OverflowError):
             return complex(np.nan, np.nan)
-    wrapper_func = np.vectorize(wrapper_func)
+    wrapper_func = np.vectorize(wrapper_func, otypes=[complex])
 
     try:
         return wrapper_func(f1, *args)
     except Exception as err:
+        if f2 is None:
+            raise RuntimeError(
+                "Impossible to evaluate the provided numerical function")
         warnings.warn(
             "The evaluation with %s failed.\n" % (
                 "NumPy/SciPy" if not modules else modules) +
@@ -803,7 +808,7 @@ class LineOver1DRangeSeries(Line2DBaseSeries):
 
         def func(f, imag, x):
             try:
-                w = complex(f(x + 1j * imag))
+                w = complex(f(x + 1j * imag if self.is_complex else x))
                 return w.real, w.imag
             except (ZeroDivisionError, OverflowError):
                 return np.nan, np.nan

tests/test_series.py
@@ -21,7 +21,7 @@ from spb.series import (
 )
 from sympy import (
     latex, exp, symbols, Tuple, I, pi, sin, cos, tan, log, sqrt,
-    re, im, arg, frac, Plane, Circle, Point, Sum, S
+    re, im, arg, frac, Plane, Circle, Point, Sum, S, Abs, lambdify
 )
 from sympy.external import import_module
 from sympy.vector import CoordSys3D, gradient
@@ -2811,3 +2811,33 @@ def test_show_in_legend_lines():
     s = Parametric3DLineInteractiveSeries([cos(x), sin(x), x], [(x, 0, 1)],
         "test", show_in_legend=False)
     assert not s.show_in_legend
+
+
+def test_particular_case_1():
+    # Verify that symbolic expressions and numerical lambda functions are
+    # evaluated with the same algorithm. In particular, uniform evaluation
+    # is going to use np.vectorize, which correctly evaluates the following
+    # mathematical function.
+    def do_test(a, b):
+        d1 = a.get_data()
+        d2 = b.get_data()
+        for t, v in zip(d1, d2):
+            assert np.allclose(t, v)
+
+    n = symbols("n")
+    a = S(2) / 3
+    epsilon = 0.01
+    xn = (n**3 + n**2)**(S(1)/3) - (n**3 - n**2)**(S(1)/3)
+    expr = Abs(xn - a) - epsilon
+    math_func = lambdify([n], expr)
+    s1 = LineOver1DRangeSeries(expr, (n, -10, 10), "",
+        adaptive=True, adaptive_goal=0.2)
+    s2 = LineOver1DRangeSeries(math_func, ("n", -10, 10), "",
+        adaptive=True, adaptive_goal=0.2)
+    do_test(s1, s2)
+
+    s3 = LineOver1DRangeSeries(expr, (n, -10, 10), "",
+        adaptive=False, n=10)
+    s4 = LineOver1DRangeSeries(math_func, ("n", -10, 10), "",
+        adaptive=False, n=10)
+    do_test(s3, s4)