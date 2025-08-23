bc3eb57e467151e5c749bdf2722331fee0e68fe2
Davide Sandonà <sandona.davide@gmail.com>
2025-08-23 13:28:03 +0200

    removed mayavi

--- spb/backends/mayavi/__init__.py
+++
@@ -1,1 +0,0 @@
-from spb.backends.mayavi.mayavi import MAB, MayaviBackend
--- spb/backends/mayavi/renderers/__init__.py
+++
@@ -1,4 +0,0 @@
-from spb.backends.mayavi.renderers.line3d import Line3DRenderer
-from spb.backends.mayavi.renderers.surface import SurfaceRenderer
-from spb.backends.mayavi.renderers.implicit3d import Implicit3DRenderer
-from spb.backends.mayavi.renderers.vector3d import Vector3DRenderer
